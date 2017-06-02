08ac605fb8ff18be48be54ec9672cbfb55e8f30f
Martin Bertschler <martin.bertschler@whatchado.com>
2017-06-02 10:29:17 +0200

    work with packages that are not in run/ and don't overwrite already created files - closes #1

--- run.go
+++ run.go
@@ -21,7 +21,8 @@ func main() {
 		return
 	}
 	var pkg = os.Args[1]
-	err := runCmd("go install run/" + pkg)
+	pkg = findPackageFolder(pkg)
+	err := runCmd("go install " + pkg)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -32,8 +33,27 @@ func main() {
 	}
 }
 
+func findPackageFolder(pkg string) string {
+	gopath := os.Getenv("GOPATH")
+	gopaths := strings.Split(gopath, ":")
+	for _, gp := range gopaths {
+		dir := filepath.Join(gp, "src", "run", pkg)
+		info, err := os.Stat(dir)
+		if err == nil && info.IsDir() {
+			return "run/" + pkg
+		}
+		dir = filepath.Join(gp, "src", pkg)
+		info, err = os.Stat(dir)
+		if err == nil && info.IsDir() {
+			return pkg
+		}
+	}
+	fmt.Println("couldn't find the source folder for the package", pkg)
+	os.Exit(1)
+	return ""
+}
+
 func runCmd(cmd string) error {
-	// log.Println(cmd)
 	parts := strings.Fields(cmd)
 	c := exec.Command(parts[0], parts[1:]...)
 	c.Stdout = os.Stdout
@@ -53,7 +73,12 @@ func newPackage(name string) {
 	gopath = strings.Split(gopath, ":")[0]
 	dir := filepath.Join(gopath, "src", "run", name)
 	file := filepath.Join(gopath, "src", "run", name, name+".go")
-	err := os.MkdirAll(dir, 0755)
+	info, err := os.Stat(file)
+	if err == nil && !info.IsDir() {
+		fmt.Println("The file", file, "already exists, doing nothing.")
+		return
+	}
+	err = os.MkdirAll(dir, 0755)
 	if err != nil {
 		fmt.Println(err)
 		return
